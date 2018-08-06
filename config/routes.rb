881ff1e262ec2d2b72fed5410083b3097f55e8ef
jebediahelliott <jebediahelliott@gmail.com>
2018-08-06 17:34:39 -0500

    Finish form for new user. Set up sessions controller and change root route to login page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :ingredients
   resources :recipes
   resources :users
-  resource :session
-  root 'application#home'
+  resource :session, only: [:index, :create,]
+  root 'session#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
