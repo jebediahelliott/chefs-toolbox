2d740976cb8b784d87e4e8d841be1a8589e61157
jebediahelliott <jebediahelliott@gmail.com>
2018-08-06 21:56:45 -0500

    Set up recipe controller.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,43 @@
 class RecipesController < ApplicationController
 
+  before_action :set_recipe, only: [:show, :update, :destroy, :edit]
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      redirect_to new_recipe_path
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :password, :password_confirmation)
+  end
+
 end
