cfd83f11440671f080af2d62235f16e489e19b3e
jebediahelliott <jebediahelliott@gmail.com>
2018-08-06 21:49:01 -0500

    Set up show action in user controller and start template.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      redirect_to new_user_path
+    end
   end
 
   def edit
@@ -18,14 +23,20 @@ class UsersController < ApplicationController
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   def destroy
 
   end
 
+  private
+
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
