6aa4dc8aa4f806fba4f3ccb7350a553fd4595cac
jebediahelliott <jebediahelliott@gmail.com>
2018-09-01 12:31:48 -0400

    set up menu migration and association

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,10 @@
 class Menu < ApplicationRecord
+  has_many :meals
 end
+
+
+# name
+# menu has meal attributes i.e. Monday lunch, Monday dinner, etc.
+# each meal has a description
+# each meal has associated recipes
+# recipes are timestamped based on last time used in a menu
