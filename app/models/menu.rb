d8038a0db1f295ca3b8ee50180c25f0496cfd453
jebediahelliott <jebediahelliott@gmail.com>
2018-09-01 12:50:38 -0400

    adjust menu migration, set up association between menu and recipe.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,10 +1,10 @@
 class Menu < ApplicationRecord
-  has_many :meals
+  has_many :recipes
 end
 
 
 # name
 # menu has meal attributes i.e. Monday lunch, Monday dinner, etc.
 # each meal has a description
-# each meal has associated recipes
+# each meal has associated recipes(select recipes to associate with meal/menu)
 # recipes are timestamped based on last time used in a menu
--- db/migrate/20180901153720_create_menus.rb
+++ db/migrate/20180901153720_create_menus.rb
@@ -2,6 +2,20 @@ class CreateMenus < ActiveRecord::Migration[5.2]
   def change
     create_table :menus do |t|
       t.string :name
+      t.string :monday_lunch
+      t.string :monday_dinner
+      t.string :tuesday_lunch
+      t.string :tuesday_dinner
+      t.string :wednesday_lunch
+      t.string :wednesday_dinner
+      t.string :thursday_lunch
+      t.string :thursday_dinner
+      t.string :friday_lunch
+      t.string :friday_dinner
+      t.string :saturday_lunch
+      t.string :saturday_dinner
+      t.string :sunday_lunch
+      t.string :sunday_dinner
       t.timestamps
     end
   end
