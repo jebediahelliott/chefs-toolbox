0c0a7db8ad3b8b468033819c56701b41e738bd0a
jebediahelliott <jebediahelliott@gmail.com>
2018-09-01 14:08:19 -0400

    set up menus controller

---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,47 @@
+class MenusController < ApplicationController
+  before_action :set_menu, only: [:show, :update, :destroy, :edit]
+
+  def index
+    @menus = Menu.all
+  end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to @menu
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+    @menu = Menu.find(params[:id])
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_menu
+    @menu = Menu.find(params[:id])
+  end
+
+  def menu_params
+    params.require(:menu).permit(:name, :monday_lunch, :monday_dinner, :tuesday_lunch, :tuesday_dinner, :wednesday_lunch, :wednesday_dinner, :thursday_lunch, :thursday_dinner, :friday_lunch, :friday_dinner, :saturday_lunch, :saturday_diner, :sunday_lunch, :sunday_dinner)
+  end
+
+end
