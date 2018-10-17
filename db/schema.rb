fb1aec041ed6c77f816e24a74cf98c8ffa93e952
jebediahelliott <jebediahelliott@gmail.com>
2018-10-17 19:27:55 -0400

    Change all coffee files to js. Add jquery gem. Add index view for inventory.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_02_182241) do
+ActiveRecord::Schema.define(version: 2018_10_16_104953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_09_02_182241) do
     t.string "unit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "inventory_amount"
   end
 
   create_table "inventories", force: :cascade do |t|
