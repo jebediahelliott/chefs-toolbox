c74b610a835f9379d34d8bf75c3f87b3cf4f79f2
jebediahelliott <jebediahelliott@gmail.com>
2018-10-19 06:32:16 -0400

    Set up inventory controller. Add container to inventory/index view.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,3 +1,41 @@
 class IngredientsController < ApplicationController
-  
+  before_action :set_ingredient, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @ingredients = Ingredient.all
+    render json: @ingredients
+  end
+
+  def create
+    @ingredient = Ingredient.create(ingredient_params)
+    render json: @ingredient, status 201
+  end
+
+  def update
+    @ingredient.update(ingredient_params)
+    render json: @ingredient
+  end
+
+  def edit
+
+  end
+
+  def show
+    render json: @ingredient
+  end
+
+  def destroy
+    @ingredient.delete
+  end
+
+  private
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :unit, :inventory_amount)
+  end
 end
