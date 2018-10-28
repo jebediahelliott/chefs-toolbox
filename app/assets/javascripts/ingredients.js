dee4a18c6dd2e748e8e3620e5e3b149a81a3cf94
jebediahelliott <jebediahelliott@gmail.com>
2018-10-28 19:15:41 -0400

    Put table structure back in view. Restructure js file into class format. Add JSON rendering to controller.

--- app/assets/javascripts/ingredients.js
+++ app/assets/javascripts/ingredients.js
@@ -1,60 +1,71 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+class Ingredient {
+  constructor(name, unit, inventory_amount) {
+    this.name = name;
+    this.unit = unit;
+    this.inventory_amount = inventory_amount;
+  }
 
-$(function() {
+  tableFormat() {
+    return `<tr>
+      <td>${this.name}</td>
+      <td>${this.inventory_amount} ${this.unit}</td>
+    </tr>`
+  }
+
+  static inventoryTable() {
+    $.get("/ingredients", function(result) {
+      let ingredients = result["data"];
+      console.log(ingredients);
+    });
+  }
+}
 
 
-});
+
 //function to populate table with ingredients
-function populateInventory() {
-  $.get("/ingredients", function(result) {
-    //set up page layout
-    $('#invPage').html(
-      `<table class="inventory">
-        <tr>
-          <th>Ingredient Name</th>
-          <th>Amount On Hand</th>
-        </tr>
-      </table>
-      <button type="button" onclick="newIngredient()">Add Ingredient</button>`
-    );
-    //populate table
-    result["data"].forEach(function(ingredient) {
-      const name = ingredient["attributes"]["name"]
-      const amount = ingredient["attributes"]["inventory-amount"]
-      const unit = ingredient["attributes"]["unit"]
-      $('.inventory').append(
-        `<tr>
-          <td>${name}</td>
-          <td>${amount} ${unit}</td>
-        </tr>`
-      );
-    })
-  });
-}
-
-//Serialize info from new ingredientn form, create new ingredient, display inventory with new ingredient
-function newIngredient() {
-  $.get('/ingredients/new', function(result) {
-    ingredientForm();
-  })
-  $('#ingForm').submit(function(event) {
-    event.preventDefault();
-    const values = $(this).serialize();
-    $.post('/ingredients', values).done(function() {
-      populateInventory();
-    })
-  });
-}
-
-//generate form to add an ingredient to the inventory
-function ingredientForm() {
-  $('#invPage').html(
-    `<form id="ingForm" action="/ingredients" method="post">
-      Name: <input name="ingredient[name]" type="text"><br>
-      Amount: <input name="ingredient[inventory_amount]" type="text">
-      Unit: <input name="ingredient[unit]" type="text"><br>
-      <input type="submit" value="Create Ingredient">
-    </form>`
-  );
-}
+// function populateInventory() {
+//   $.get("/ingredients", function(result) {
+//       ingredients = result["data"]
+//     // populate table
+//     ingredients.forEach(function(ingredient) {
+//       const name = ingredient["attributes"]["name"]
+//       const amount = ingredient["attributes"]["inventory-amount"]
+//       const unit = ingredient["attributes"]["unit"]
+//       // debugger
+//       $('.inventory').append(
+//         `<tr>
+//           <td>${name}</td>
+//           <td>${amount} ${unit}</td>
+//         </tr>`
+//       );
+//     })
+//   });
+// }
+//
+// //Serialize info from new ingredientn form, create new ingredient, display inventory with new ingredient
+// function newIngredient() {
+//   $.get('/ingredients/new', function(result) {
+//     ingredientForm();
+//   })
+//   $('#ingForm').submit(function(event) {
+//     event.preventDefault();
+//     const values = $(this).serialize();
+//     $.post('/ingredients', values).done(function() {
+//       populateInventory();
+//     })
+//   });
+// }
+//
+// //generate form to add an ingredient to the inventory
+// function ingredientForm() {
+//   $('#invPage').html(
+//     `<form id="ingForm" action="/ingredients" method="post">
+//       Name: <input name="ingredient[name]" type="text"><br>
+//       Amount: <input name="ingredient[inventory_amount]" type="text">
+//       Unit: <input name="ingredient[unit]" type="text"><br>
+//       <input type="submit" value="Create Ingredient">
+//     </form>`
+//   );
+// }
